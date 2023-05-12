40465d852521b0e32de9611272c179b66e60da33
NetEugene <gmsebperson@gmail.com>
2023-05-12 19:26:27 +0800

    FinalTask

--- task_final/solution.js
+++ task_final/solution.js
@@ -31,23 +31,23 @@
 //         }
 //      ]
 //    }
-// }
+// } 'ул. Академика Королёва, дом ,12 1 подъезд ,15 этаж, кв. 253' to equal 'ул. Академика Королёва, дом 12, 1 подъезд, 15 этаж, кв. 253'.
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client:'', goods: [], order: {address:'', sum:0.00}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+        }
+        
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = 'ул.'+' '+address.street +', дом '+ address.house + ', '+ address.entrance+' подъезд, '+ address.floor+' этаж, кв. '+ address.flat;
+    data.order.sum = sum;
+    data.client = name +' '+ phone;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data: data});
+   
     return jsonData;
 }
